fbd33b314e978b1c5337c4328762064db61064f7
Binary&&Entropy <kevinhu253216@gmail.com>
2018-07-07 17:34:13 -0400

    Update TCPServer.java

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -2,7 +2,7 @@ import java.io.DataInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class TCPServer {
+public class TCPServer { //从客户端口读数据
 
     public static void main(String[] args)throws Exception {
         ServerSocket ss = new ServerSocket(6666);
